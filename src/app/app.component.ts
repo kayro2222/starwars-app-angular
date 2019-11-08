303c8f349b5217671db1370281a5beb8bd4b7832
Kayro Mizael <kayromsa@gmail.com>
2019-11-08 19:56:18 -0300

    fix commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-starwars';
+export class AppComponent implements OnInit {
+
+  people: any = [];
+  nextPage: any;
+  previousPage: any;
+  homeWorld: any = [];
+
+  private url: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.url = 'https://swapi.co/api/people/';
+    this.getData(this.url);
+  }
+
+  nextData(){
+    this.url = this.nextPage;
+    this.getData(this.url);
+  }
+
+  previousData(){
+    this.url = this.previousPage;
+    this.getData(this.url);
+  }
+
+  async getData(url) {
+    await this.http.get(url).toPromise().then(data => {
+      this.people = data['results'];
+      this.nextPage = data['next'];
+      this.previousPage = data['previous'];
+
+      for (let i = 0; i < Object.keys(this.people).length; i++) {
+        let res = this.people[i];
+
+        res = res['homeworld'];
+
+        this.http.get(res).toPromise().then(planet => {
+          this.homeWorld[i] = planet['name'];
+        });
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
